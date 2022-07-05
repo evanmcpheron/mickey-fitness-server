2721037599b622892a4d9894cc674be15549e105
Evan McPheron <evan.mcpheron@icloud.com>
2022-07-05 08:54:36 -0700

    changes to package.json

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-import express from "express";
-import cors from "cors";
-import path from "path";
-import bodyParser from "body-parser";
-import connectDB from "./v1/Config/db";
-import cookieSession from "cookie-session";
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import bodyParser from 'body-parser';
+import connectDB from './v1/Config/db';
+import cookieSession from 'cookie-session';
 
-const stripe = require("stripe")(process.env.STRIPE_SK);
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 const PORT = process.env.PORT || 8080;
 
 const application = {
-  app,
+	app,
 };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(
-  cookieSession({
-    signed: false,
-    // secure: process.env.NODE_ENV !== "test",
-    secure: false,
-  })
+	cookieSession({
+		signed: false,
+		// secure: process.env.NODE_ENV !== "test",
+		secure: false,
+	})
 );
 
 // app.get("/v1/secret", async (req, res) => {
@@ -36,22 +36,13 @@ app.use(
 
 //   res.json({ client_secret: intent.client_secret }); // ... Fetch or create the PaymentIntent
 // });
-app.use("/v1", require("./v1/Routes/index")(application));
+app.use('/v1', require('./v1/Routes/index')(application));
 
-// SERVE REACT APP IN PRODUCTION
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, async () => {
-    await connectDB();
-    console.log(`Server listening on http://localhost:${PORT}`);
-  });
+if (process.env.NODE_ENV !== 'test') {
+	app.listen(PORT, async () => {
+		await connectDB();
+		console.log(`Server listening on http://localhost:${PORT}`);
+	});
 }
 
 module.exports = app;
